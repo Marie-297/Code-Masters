fb57dc47cf9971a7b69a48bbfa70ffeaf8605784
Mariama Seidu <153280802+Marie-297@users.noreply.github.com>
2024-03-09 16:11:41 +0000

    created a page to display your status and moves when all cards matched, and made some few changes to make it more interactive

--- memory Game. js/index.js
+++ memory Game. js/index.js
@@ -1,46 +1,52 @@
-
-const emojis = ['🍕','🍕','😍','😍','😈','😈','🥶','🥶','🤑','🤑','👾','👾','❤','❤','👽','👽'];
-var shuf_emojis = emojis.sort(() => (Math.random() > .5) ? 2 : -1);
-for ( var i =0; i<emojis.length; i++){
-   let box = document.createElement('div')
-   box.className ='items';
-   box.innerHTML = shuf_emojis[i]
-
-   //    game funtion js 
-   box.onclick = function(){
-       moveCard()
-       this.classList.add('boxOpen')
-       setTimeout(function(){
-           if(document.querySelectorAll('.boxOpen').length > 1){
-               if(document.querySelectorAll('.boxOpen')[0].innerHTML == document.querySelectorAll('.boxOpen')[1].innerHTML){
-                   document.querySelectorAll('.boxOpen')[0].classList.add('boxMatch')
-                   document.querySelectorAll('.boxOpen')[1].classList.add('boxMatch')
-                   
-                   document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
-                   document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
-
-                   if(document.querySelectorAll('.boxMatch').length == emojis.length){
-                       alert('YOU WON')
-                   }
-               } else {
-                //add some shake animations in the our style.css to the cards when clicked and unmatched
-                  document.querySelectorAll('.boxOpen')[0].classList.add('shake');
-                  document.querySelectorAll('.boxOpen')[1].classList.add('shake');
-                   document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
-                   document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
-               }
-           }
-       },500) 
-   }
-
-   document.querySelector('.game').appendChild(box);
-}
-
 const moves = document.querySelector("#moves-count")
 const timeValue = document.querySelector("#time")
 const controlCont = document.querySelector(".controls-container")
+let resetBtn = document.querySelector(".Reset")
+let startBtn = document.querySelector(".startbutton")
 const gameCont = document.querySelector(".game")
 const result = document.querySelector("#result")
+
+const emojis = ['🍕','🍕','😍','😍','😈','😈','🥶','🥶','🤑','🤑','👾','👾','❤','❤','👽','👽'];
+var shuf_emojis = emojis.sort(() => (Math.random() > .5) ? 2 : -1);
+//I created a function to play the game so I have put everything in a function to run it only when you start the game.
+function playGame() {
+  for ( var i =0; i<emojis.length; i++){
+    let box = document.createElement('div')
+    box.className ='items';
+    box.innerHTML = shuf_emojis[i]
+ 
+    //    game funtion js 
+    box.onclick = function(){
+        moveCard()
+        this.classList.add('boxOpen')
+        setTimeout(function(){
+            if(document.querySelectorAll('.boxOpen').length > 1){
+                if(document.querySelectorAll('.boxOpen')[0].innerHTML == document.querySelectorAll('.boxOpen')[1].innerHTML){
+                    document.querySelectorAll('.boxOpen')[0].classList.add('boxMatch')
+                    document.querySelectorAll('.boxOpen')[1].classList.add('boxMatch')
+                    
+                    document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
+                    document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
+ 
+                    if(document.querySelectorAll('.boxMatch').length == emojis.length){
+                      gameOver();
+                    }
+                } else {
+                 //add some shake animations in the our style.css to the cards when clicked and unmatched
+                   document.querySelectorAll('.boxOpen')[0].classList.add('shake');
+                   document.querySelectorAll('.boxOpen')[1].classList.add('shake');
+                    document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
+                    document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
+                }
+            }
+        },500) 
+    }
+ 
+    gameCont.appendChild(box);
+ }
+}
+
+
 //Initial Time
 let seconds = 0;
 let minutes = 0;
@@ -65,21 +71,34 @@ function moveCard() {
   moveCount = moveCount + 1;
   moves.innerHTML = `<span>Moves:</span>${moveCount}`;
 }
-//finish game conditions... Yet to work on it
-// function gameOver() {
-//   if (minutesValue.innerText == "01" && secondsValue.innerText == "00") {
-//     controlCont.classList.remove("hidden")
-//     gameCont.classList.add("hidden")
-//     result.innerHTML = "GAME OVER"
-//     clearInterval(interval);
-//   }
-// }
+// when all cards are matched it opens a page that tells you YOU WIN and the number of moves you made to complete the game
+function gameOver() {
+  gameCont.classList.add("hidden")
+  controlCont.classList.remove("hidden")
+  resetBtn.classList.add("hidden");
+  startBtn.classList.add("hidden");
+  moves.classList.add("hidden");
+  result.innerHTML = `<h4>Moves:${moveCount}</h4>`
+  clearInterval(interval);
+}
 
 function startGame() {
   //movesCount = 0;
   seconds = 0;
   minutes = 0;
+  //play the game
+  playGame();
   //Start timer
   interval = setInterval(timeGenerator, 1000);
-  gameOver();
+  //gameOver();
+}
+//yet to work on it
+// function playAgain() {
+//   location.reload();
+//   window.onload = playGame();
+// }
+
+// refreshes the page and takes you back to starting the game
+function BacktoGame() {
+ location.reload();
 }
